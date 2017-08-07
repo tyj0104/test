12a5553e0e4746baa9369f381f8bea0f04b4e915
admin <admin>
2017-08-07 23:36:49 +0800

    第三次提交

--- app/src/main/java/com/example/jay/learn22/MainActivity.java
+++ app/src/main/java/com/example/jay/learn22/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("第一次学习");
         System.out.println("第2次学习");
+        System.out.println("第3次学习");
 
     }
 }
