bd9e39cf0d49ad2c07fb00d15d1c7dcd777310af
tyj <254103715@qq.com>
2017-08-08 18:19:57 +0800

    第一次提交

--- app/src/main/java/com/example/jay/learn22/MainActivity.java
+++ app/src/main/java/com/example/jay/learn22/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("第一次学习");
+        System.out.println("第2次学习");
     }
 }
