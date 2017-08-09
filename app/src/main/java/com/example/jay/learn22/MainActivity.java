f2f8d5fb831189606988e65a57ed59067b602467
tyj <254103715@qq.com>
2017-08-09 10:41:44 +0800

    “update“

--- app/src/main/java/com/example/jay/learn22/MainActivity.java
+++ app/src/main/java/com/example/jay/learn22/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("ksks。.sout");
+        System.out.println("keeee");
+        System.out.println("00000");
+
         System.out.println("ksks。.sogggggut");
     }
 }
